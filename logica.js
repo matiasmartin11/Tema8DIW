a4b09c187e5dea4af86724a957b1ef2dee5002c8
matiasmartin11 <mst846@gmail.com>
2022-01-11 20:23:27 +0100

    ultima actualizacion

--- logica.js
+++ logica.js
@@ -1,23 +1,33 @@
-let nodo = document.querySelector("character");
-
+let nodo = document.querySelector(".seleccionado");
+let psjSeleccionado;
 document.addEventListener("keydown", manejarEvento);
 
-document.addEventListener("keyup", () => {
+/*document.addEventListener("keyup", () => {
     nodo.removeAttribute("class");
     nodo.classList.add("character");
     console.log("keyup");
 });
+*/
+function seleccionarPsj(){
+
+}
 
 function manejarEvento(evento){
+    //nombre del personaje + atacar
+    ordenAtaque = nodo.classList[1] + "Ataca";
+    
+    console.log(nodo);
+    console.log(nodo.classList[1]+ "Ataca");
     console.log(evento);
     console.log("entra");
 
     if(evento.keyCode === 32){
-        nodo.removeAttribute("class");
+        nodo.classList.add(ordenAtaque);
+        
+        console.log(nodo.classList);
         setTimeout(function(){
-            nodo.classList.add("agacharse");
-        },2000);
-        nodo.removeAttribute("class");
-        nodo.classList.add("flexiones");
+            nodo.classList.remove(ordenAtaque);
+        },1000);
+        
     }
 }
