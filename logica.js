39a3871b4fb912877369d1ab91adba19e75c5eff
Miriam1169 <74782952+Miriam1169@users.noreply.github.com>
2022-01-11 19:44:55 +0100

    hihi

--- logica.js
+++ logica.js
@@ -21,3 +21,40 @@ function manejarEvento(evento){
         nodo.classList.add("flexiones");
     }
 }
+
+function iniciar(){
+    selectAvatar();
+
+    var tablaAvatar=getNode("personajes");
+
+    var celdasAvatar=tablaAvatar.getElementsByTagName("td");
+    for (var i=0;i<celdasAvatar.length;i++){
+		crearEvento(celdasAvatar[i],"click",detectarPersonajes);
+	}
+}
+function selectAvatar(){
+    getNode("zonaPersonaje").appendChild(nuevaTabla);
+}
+function detectarPersonajes(){
+    for (var i=0; i<this.parentElement.children.length;i++){	
+		if (navigator.appName.indexOf("Explorer") != -1){
+			this.parentNode.childNodes[i].className = this.parentNode.childNodes[i].className.replace(/\bseleccionado\b/,'');
+		}else{
+			this.parentElement.children[i].classList.remove("seleccionado");
+		}
+	}
+
+	if (navigator.appName.indexOf("Explorer") != -1){
+		colorActivo=this.className;
+		this.className+=" seleccionado";
+	}else{
+		colorActivo=this.classList[0];
+		this.classList.add("seleccionado");
+	}
+}
+crearEvento(window,"load", iniciar);
+
+
+
+
+
